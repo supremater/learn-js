dabddb1b88705b3fc416abc3b56eb9d2d6761899
GeorgJs <58216452+GeorgJs@users.noreply.github.com>
2019-12-06 19:51:38 +0300

    sunsky777

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+let nameCar = 'Ford'
+let carYear = 2008
+
+
+if ((2018 - carYear) < 10) {
+    console.log('Возраст машины меньше 10 лет')
+} else {
+    console.log('Возраст машины больше 10 лет')
+}
