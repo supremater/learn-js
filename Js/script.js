9b00874f9fbcf1cde461f950fc37fc345803749a
GeorgJs <58216452+GeorgJs@users.noreply.github.com>
2019-12-17 15:17:16 +0300

    45.3836

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,22 @@
-function Clicked() {
-    const result = prompt('Add the word');
+const user = {
+    firstName: 'Goward',
+    age: 30,
+    isAdmin: true,
+    email: 'gamedev@gmail.com',
+    'user-address': {
+        city: "Moscov"
+    },
+    skils: ['html', 123 , false]
+};
 
-    if (result == 'Ford') {
-        alert("Succses");
-    } else { 
-        alert('not');
-    }
-}
+let value;
+let prop = 'email';
+
+value = user[prop];
+user['user-address'].city = 'Riga';
+
+const a = [1, 2, 3, 4, 5];
+
+a.forEach();
+
+
