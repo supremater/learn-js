326ead7293571881759b3ef38b94144647b045f0
GeorgJs <58216452+GeorgJs@users.noreply.github.com>
2020-05-03 13:46:31 +0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,20 +1,3 @@
-let str1 = 'Hello world'
-let str2 = 'Hello world 2'
+var a = "Привет"
 
-let personName = 'Виктор'
-
-let message = 'Человека зовут "' + personName + '"'
-let message2 = 'Человека зовут \'' + personName + '\''
-
-
-console.log(message2); 
-
-
-let newMessage = 'Hello world!!!'
-
-console.log(newMessage.length);
-console.log(newMessage.toUpperCase());
-console.log(newMessage.toLowerCase());
-console.log(newMessage.charAt(0));
-console.log(newMessage.indexOf('world'));
-console.log(newMessage.substring(1, 4 ))
+console.log(a);
