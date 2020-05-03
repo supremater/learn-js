1d6a4ceb53337c967ac48017a5adf5f83d1d9ede
GeorgJs <58216452+GeorgJs@users.noreply.github.com>
2020-05-03 13:49:02 +0300

    Create vscode.js

---
+++ Настройки/vscode.js
@@ -0,0 +1,79 @@
+{
+    "editor.formatOnPaste": true,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.snippetSuggestions": "top",
+    "window.zoomLevel": 0,
+    "files.autoSave": "onFocusChange",
+    "editor.minimap.maxColumn": 80,
+    "workbench.activityBar.visible": true,
+    "editor.renderControlCharacters": false,
+    "window.menuBarVisibility": "default",
+    "workbench.iconTheme": "material-icon-theme",
+    "emmet.triggerExpansionOnTab": true,
+    "editor.scrollBeyondLastColumn": 10,
+    "editor.wordWrap": "on",
+    "editor.folding": false,
+    "editor.glyphMargin": false,
+    "editor.smoothScrolling": true,
+    "editor.fastScrollSensitivity": 6,
+    "editor.mouseWheelScrollSensitivity": 2,
+    "editor.renderLineHighlight": "none",
+    "workbench.statusBar.visible": true,
+    "[html]": {
+        "editor.defaultFormatter": "vscode.html-language-features"
+    },
+    "terminal.integrated.cursorStyle": "line",
+    "terminal.integrated.fontSize": 17,
+    "explorer.confirmDragAndDrop": false,
+    "px-to-rem.notify-if-no-changes": false,
+    "git.autofetch": true,
+    "liveServer.settings.donotShowInfoMsg": true,
+    "diffEditor.ignoreTrimWhitespace": true,
+    "oneDarkPro.italic": false,
+    "[javascript]": {
+        "editor.defaultFormatter": "vscode.typescript-language-features"
+    },
+    "vim.easymotion": true,
+    "vim.incsearch": true,
+    "vim.useSystemClipboard": true,
+    "vim.useCtrlKeys": true,
+    "vim.hlsearch": true,
+    "vim.insertModeKeyBindings": [
+        {
+        "before": ["j", "j"],
+        "after": ["<Esc>"]
+        }
+    ],
+    "vim.normalModeKeyBindingsNonRecursive": [
+        {
+        "before": ["leader>", "d"],
+        "after": ["d", "d"]
+        },
+        {
+        "before": ["<C-n>"],
+        "commands": [":nohl"]
+        }
+    ],
+    "vim.leader": "<space>",
+    "vim.handleKeys": {
+        "<C-a>": false,
+        "<C-f>": false
+    },
+    "html.autoClosingTags": false,
+    "vim.easymotionMarkerFontSize": "15",
+    "terminal.integrated.shell.windows": "C:\\Windows\\System32\\cmd.exe",
+    "[json]": {
+    
+    },
+    "gitlens.hovers.currentLine.over": "line",
+    "workbench.colorTheme": "Monokai Dark Soda",
+    "editor.fontFamily": "Fira Code",
+    "terminal.integrated.fontWeightBold": "normal",
+    "debug.console.fontSize": 15,
+    "editor.fontLigatures": 1,
+    "terminal.integrated.fontFamily": "monospace",
+    "editor.fontSize": 16,
+    "launch": {
+    
+    }
+}
